1d94d0484d98690ac576d3061456cd734b8960e3
jihad710 <dvfaruq@insaaftechsolutions.com>
2024-03-06 14:11:52 +0600

    Add food category swiper component

---
+++ src/Pages/Home/Category/Category.jsx
@@ -0,0 +1,62 @@
+import "swiper/css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import slide1 from "../../../assets/FoodCategory/slide1.jpg";
+import slide2 from "../../../assets/FoodCategory/slide2.jpg";
+import slide3 from "../../../assets/FoodCategory/slide3.jpg";
+import slide4 from "../../../assets/FoodCategory/slide4.jpg";
+import slide5 from "../../../assets/FoodCategory/slide5.jpg";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const Category = () => {
+  return (
+    <section>
+      <SectionTitle
+        subHeading={"From 11.00am to 10.00pm"}
+        heading={"Order Online"}
+      ></SectionTitle>
+
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        className="mySwiper mb-24"
+      >
+        <SwiperSlide>
+          <img src={slide1} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            Salads
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            Soup
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            Pizzas
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            Desserts
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            Salads
+          </h3>
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
+};
+
+export default Category;
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   );
   return (
     <>
-      <div className="navbar bg-black bg-opacity-30 text-white max-w-screen-xl fixed  z-10">
+      <div className="navbar bg-black  text-white max-w-screen-xl fixed  z-10">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
